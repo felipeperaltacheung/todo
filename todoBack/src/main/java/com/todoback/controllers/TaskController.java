288ce1310cafbd307491eb9881408d7a3b731c69
felipeperaltacheung <f33___@hotmail.com>
2023-02-23 16:23:43 -0300

    Pojo Task + domain/controller/repository

--- todoBack/src/main/java/com/todoback/controllers/TaskController.java
+++ todoBack/src/main/java/com/todoback/controllers/TaskController.java
@@ -1,4 +1,70 @@
 package com.todoback.controllers;
 
+import com.todoback.model.Task;
+import com.todoback.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task){
+        return taskRepository.save(task);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTask(@PathVariable Long id) {
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            return ResponseEntity.ok(task.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            Task existingTask = task.get();
+            existingTask.setTitle(updatedTask.getTitle());
+            existingTask.setDescription(updatedTask.getDescription());
+            existingTask.setCompleted(updatedTask.isCompleted());
+            return ResponseEntity.ok(taskRepository.save(existingTask));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTask(@PathVariable Long id) {
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            taskRepository.delete(task.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/completed")
+    public List<Task> getCompletedTasks() {
+        return taskRepository.findByCompleted(true);
+    }
+
+
 }
--- todoBack/src/main/java/com/todoback/repositories/TaskRepository.java
+++ todoBack/src/main/java/com/todoback/repositories/TaskRepository.java
@@ -1,4 +1,12 @@
 package com.todoback.repositories;
 
-public class TaskRepository {
+import com.todoback.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByCompleted(boolean completed);
 }
